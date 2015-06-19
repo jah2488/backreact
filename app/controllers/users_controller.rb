ec36bd667ed874b969e6d5eadd31014b7a70d0c4
xiawei <graciexia@outlook.com>
2015-06-19 16:25:21 -0500

    change userscontroller1

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,45 @@
+class UsersController < ApplicationController
+  def index
+    render json: User.all
+  end
+
+  def show
+    render json: User.where(zip_code: params[:zip_code])
+  end
+
+
+  def create
+    begin
+      user = User.create(username: params[:username], email:params[:email], password:params[:password], password_confirmation: params[:password_confirmation], phone_number: params[:phone_number], address: params[:address], zip_code: params[:zip_code])
+      render json: user
+    rescue ActionController::ParameterMissing => error
+      render json: { error: error.message }, status: 422
+    end
+  end
+
+
+def update
+  if User.exists?(params[:id])
+    user = User.find(params[:id])
+    updated_user = User.update(user_params)
+       render json: updated_user
+ else
+    render json: { error: 'User not found' }, status: 404
+  end
+end
+
+
+  def destroy
+    if User.exists?(params[:id])
+    deleted_user = User.destroy (params[:id])
+    render json: deleted_user
+    else
+      render json: { error: 'User not found' }, status: 404
+    end
+  end
+
+private
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :password_confirmation, :phone_number, :address, :zip_code)
+  end
+end
