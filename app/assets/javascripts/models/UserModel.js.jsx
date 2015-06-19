9f816d763b3668ba0e3f185d0515c966932d659a
maryna <luv_this_world@hotmail.com>
2015-06-19 16:47:16 -0500

    fixed error

--- app/assets/javascripts/models/UserModel.js.jsx
+++ app/assets/javascripts/models/UserModel.js.jsx
@@ -14,11 +14,11 @@ var User = Backbone.Model.extend({
 			return 'Please enter your username';
 		} else if(!attr.password) {
 			return 'Please enter your password';
-		} else if (!validator.isLength((attr.password), 6) {
+		} else if(!validator.isLength((attr.password), 6)) {
 			return 'Your password be at least 6 characters';
 		} else if(!attr.password_confirmation) {
 			return 'Please enter your password';
-		} else if (!validator.isLength((attr.password_confirmation), 6) {
+		} else if (!validator.isLength((attr.password_confirmation), 6)) {
 			return 'Your password be at least 6 characters';
 		} else if(!attr.email) {
 			return 'Please enter your email';
@@ -32,7 +32,7 @@ var User = Backbone.Model.extend({
 			return 'Please enter your ZIP Code';
 		} else if(!validator.isNumeric(attr.zip_code)) {
 			return 'Your ZIP Code should only contain numbers';
-		} else if(!validator.isLength((attr.zip_code), 5) {
+		} else if(!validator.isLength((attr.zip_code), 5)) {
 			return 'ZIP Code should be 5 characters long';
 		} else { return false; }
 	}
