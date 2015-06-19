bf9520228cd16d4316416b87248ae0c3b4087f85
xiawei <graciexia@outlook.com>
2015-06-19 18:10:44 -0500

    Fixing Syntax Error in posts controller.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,22 +7,23 @@ class PostsController < ApplicationController
     # @links_and_votes = Link.select('links.id, post, title, sum(number) as "vote_count"')
     #                        .joins('left join votes on votes.link_id = links.id')
     #                        .group('links.id')
-    begin
-      time = Time.new
-      room = Room.all
-      recent_chats = []
-      room.each do |element|
-        if (time - element.created_at) <= display_range_seconds
-          recent_chats << element
-        end
-      end
-      recent_chats = recent_chats.group_by { |room| room.user }–
-                                 .map { |rooms| rooms.first }
-      recent_chats = recent_chats - ["room master"]
-      render json: recent_chats
-    rescue Exception => error
-      render json: { error: error.message + " --- time range must be expressed as seconds >= 1."}, status: 422
-    end
+    # begin
+    #   time = Time.new
+    #   room = Room.all
+    #   recent_chats = []
+    #   room.each do |element|
+    #     if (time - element.created_at) <= display_range_seconds
+    #       recent_chats << element
+    #     end
+    #   end
+    #   recent_chats = recent_chats.group_by { |room| room.user }–
+    #                              .map { |rooms| rooms.first }
+    #   recent_chats = recent_chats - ["room master"]
+    #   render json: recent_chats
+    # rescue Exception => error
+    #   render json: { error: error.message + " --- time range must be expressed as seconds >= 1."}, status: 422
+    # end
+    render json: { message: 'Not Implemented Yet' }, status: 501
   end
 
   def show
