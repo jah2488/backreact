df396474b2d21bc2b02dd3feb325e7cd4d1fd9fd
xiawei <graciexia@outlook.com>
2015-06-20 12:48:05 -0500

    fixed merge problem to master

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,66 +1,4 @@
 class CommentsController < ApplicationController
-<<<<<<< HEAD
- # before_action :authenticate_user!, only: [:show, :edit, :update, :destroy]
-
- #  def index
- #    @comments = Comment.all
- #  end
-
- #  def show
- #    set_comment
- #  end
-
-
- #  def new
- #    @comment = Comment.new
- #    @all_users = User.select_users
- #    @all_posts = Post.select_posts
- #  end
-
-
- #  def edit
- #    set_comment
- #    @all_users = User.select_users
- #    @all_posts = Post.select_posts
- #  end
-
-
- #  def create
- #    @comment = Comment.new(comment_params)
-
- #    if @comment.save
- #      redirect_to @comment, notice: 'Comment was successfully created.'
- #    else
- #      render :new
- #    end
- #  end
-
-
- #  def update
- #    set_comment
- #    if @comment.update(comment_params)
- #      redirect_to @comment, notice: 'Comment was successfully updated.'
- #    else
- #      render :edit
- #    end
- #  end
-
-
- #  def destroy
- #    set_comment
- #    @comment.destroy
- #    redirect_to comments_url, notice: 'Comment was successfully destroyed.'
- #  end
-
- #  private
- #  def set_comment
- #    @comment = Comment.find(params[:id])
- #  end
-
- #  def comment_params
- #    params.require(:comment).permit(:body, :user_id, :post_id)
- #  end
-=======
   before_action :authenticate_user!, only: [:show, :create, :update, :destroy]
   before_action :allowed_to_modify!, only: [:update, :destroy]
   def index
@@ -99,5 +37,4 @@ class CommentsController < ApplicationController
       redirect_to :back, status: 302 # alert: "Could not find the selected comment in the DB."
     end
   end
->>>>>>> gracie
 end
