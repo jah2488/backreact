0637349d149ddd65dff87b422d44768c00df7006
carissamelanson <carissa.leos@gmail.com>
2015-06-19 22:24:39 -0500

    edits to html for logo in login page component

--- app/assets/javascripts/components/LoginPageComponent.js.jsx
+++ app/assets/javascripts/components/LoginPageComponent.js.jsx
@@ -4,9 +4,9 @@ var Login = React.createClass({
 		return (
 
 			<div className="login-page">
-				<div className="logo"alt="Iron Rides logo">
-				</div>
+				<img className="logo"src="images/iron-ride-logo.png" alt="Iron Rides logo">
 
+		
 				<form className="login-form" onSumbit={this.submitLogin}>
           			<label className="form-label"> Username 
           				<input type="text" ref="username" className="input-box" placeholder="Enter your username"/>
@@ -15,15 +15,15 @@ var Login = React.createClass({
             		<label className="password"> Password 
             			<input type="password" ref="password" className="input-box" placeholder="Enter your password"/>
             		</label>
-            	</form>
+        </form>
 
             	<button className="login-btn">
-  					<strong>Log In</strong>
-  				</button>
+  					     <strong>Log In</strong>
+  				    </button>
 
-				<button className="sign-up-btn">
-  					<strong>Sign up</strong>
-  				</button>
+				      <button className="sign-up-btn">
+  					     <strong>Sign up</strong>
+  				    </button>
 
 				<div className="error-msg">
 				</div>
