20d517aaca12a05d7c42a3f989f7ef233ea2e299
carissamelanson <carissa.leos@gmail.com>
2015-06-20 21:10:10 -0500

    created .scss for login page, edited text for welcome page, deleted practice files

--- app/assets/javascripts/components/LoginPageComponent.js.jsx
+++ app/assets/javascripts/components/LoginPageComponent.js.jsx
@@ -5,28 +5,31 @@ var Login = React.createClass({
 
 			<div className="login-page">
 				<img className='welcome-image' src="assets/home-page-visual.png" alt="Iron Rides Carpool"/>
+				<h2>Log in or sign up to start riding!</h2>
 		
 				<form className="login-form" onSumbit={this.submitLogin}>
-					<label className="form-label"> Username 
+					<label className="form-label"> Username &nbsp;
 						<input type="text" ref="username" className="input-box" placeholder="Enter your username"/>
 					</label>
-					
-					<label className="password">password
+					<br/>
+					<label className="password">Password  &nbsp;&nbsp;
 						<input type="password" ref="password" className="input-box" placeholder="Enter your password"/>
-					</label>
+					</label> 
+					<br/>
 
 					<button className="login-btn">
 						 <strong>Log In</strong>
 					</button>
+					<br/>
 
-					<button className="sign-up-btn">
+					<button className="join-btn">
 						<strong>Sign up</strong>
 					</button>
 
 					<div className="error-msg"></div>
 				</form>
 				<footer>
-					<p>Created by Gracie, Maryna and Carissa</p>
+					Created by Gracie, Maryna and Carissa
 				</footer>
 			</div>
 		)
