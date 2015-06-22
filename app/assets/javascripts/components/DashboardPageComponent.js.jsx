cf6a41cdd63be71fb6d702ce0b5805a8887d6049
carissamelanson <carissa.leos@gmail.com>
2015-06-21 20:42:47 -0500

    styled dashboard page

--- app/assets/javascripts/components/DashboardPageComponent.js.jsx
+++ app/assets/javascripts/components/DashboardPageComponent.js.jsx
@@ -11,10 +11,19 @@ var Dashboard = React.createClass({
 
 	  			<img className='welcome-image' src="assets/home-page-visual.png" alt="Iron Rides Carpool"/>
 
-	  			<h2>Post Stuff!</h2>
-	  			
-				<div className="post-box"></div>
+	  			<h2>Start the carpool conversation!</h2>
 
+	  			<form className='dashboard-form'>
+
+					<label className='form-label' ref='Comments'>Start conversations with your classmates on the message board!</label>
+					<input type='text' ref='commentsbox' className='input-box' placeholder='comments' />
+
+	  				<button className="submit-btn" onClick={this.findClassmate}>
+	  					<strong>Submit</strong>
+	  				</button>
+	  			</form>
+
+	  			
 				<footer>
 					Created by Gracie, Maryna and Carissa
 				</footer>
